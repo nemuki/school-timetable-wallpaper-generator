b7bb816f4b779e0c2cf5901e81835ed3ae7f864d
Naoki Takahashi <aho.mickey.mein@gmail.com>
2021-02-06 10:50:08 +0900

    行の追加の操作ができた

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@ const table = document.getElementById('timetable');
 
 function addRow() {
     // テーブルのインデックス 0 の行（一行目）に行を挿入
-    var newRow = table.insertRow(-1);
+    let newRow = table.insertRow(-1);
 
     // 一行目にセルを挿入
-    var newCell = newRow.insertCell(0);
+    let newCell1 = newRow.insertCell(-1);
+    let newCell2 = newRow.insertCell(-1);
 
     // 作成したセルにテキストノードを挿入
-    var newText = document.createTextNode('New top row')
-    newCell.appendChild(newText);
+    let newDate = document.createTextNode('MM/DD');
+    let newSubject = document.createTextNode('教科名');
+    newCell1.contentEditable = true;
+    newCell1.appendChild(newDate);
+    
+    newCell2.contentEditable = true;
+    newCell2.appendChild(newSubject);
 }
